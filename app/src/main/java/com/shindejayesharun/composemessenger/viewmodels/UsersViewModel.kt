90a55fffd2e22c543a4c395ab8e9a7e0673b002e
shindekalpesharun <shindekalpesharun@gmail.com>
2021-07-27 23:52:24 +0530

    view model use

--- app/src/main/java/com/shindejayesharun/composemessenger/viewmodels/UsersViewModel.kt
+++ app/src/main/java/com/shindejayesharun/composemessenger/viewmodels/UsersViewModel.kt
@@ -1,4 +1,28 @@
 package com.shindejayesharun.composemessenger.viewmodels
 
-class UsersViewModel {
+import android.provider.FontsContract
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shindejayesharun.composemessenger.data.UserApiService
+import com.shindejayesharun.composemessenger.data.models.User
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UsersViewModel: ViewModel() {
+    private val apiService= UserApiService.create()
+
+    private val _users by lazy {
+            MutableStateFlow<List<User>>(listOf()).also { userFlow ->
+                viewModelScope.launch {
+                    userFlow.value = fetchUsers(20)
+                }
+            }
+    }
+
+    private suspend fun fetchUsers(count:Int):List<User>{
+        val data = apiService.getUsers(count = count)
+        return  data.results
+    }
 }
